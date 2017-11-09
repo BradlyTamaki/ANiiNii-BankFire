1501c084879174383a7f9053bd954ed10a6fe924
bradlytamaki <bradlytamaki@gmail.com>
2017-11-09 09:25:56 -0800

    .

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { ToastController } from 'ionic-angular';
 
 @Injectable()
 export class AuthProvider {
 
-  constructor(public angularFireAuth: AngularFireAuth) {
+  constructor(public angularFireAuth: AngularFireAuth, private toastCtrl: ToastController) {
     console.log('Hello AuthProvider Provider');
+
+    
     
   }
 
