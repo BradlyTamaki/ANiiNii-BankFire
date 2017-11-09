95d1e7abe0549ec4fba497c3daf1dbee1f5ee78a
bradlytamaki <bradlytamaki@gmail.com>
2017-11-08 16:58:49 -0800

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { MyApp } from './app.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook';
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDsh_jb0r-KwZbGdAwQ7MYvmDKVKBMJKao",
+  authDomain: "aniinii-bank.firebaseapp.com",
+  databaseURL: "https://aniinii-bank.firebaseio.com",
+  projectId: "aniinii-bank",
+  storageBucket: "aniinii-bank.appspot.com",
+  messagingSenderId: "758193744087"
+}
 
 @NgModule({
   declarations: [
@@ -14,6 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,7 +38,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    Facebook
   ]
 })
 export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+import firebase from 'firebase';
 
 @IonicPage()
 @Component({
@@ -8,11 +10,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public facebook: Facebook) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+  
+  login() {
+    console.log('hi');
+    this.facebook.login(['email']).then((response) => {
+      const facebookCredential = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
 
+      firebase.auth().signInWithCredential(facebookCredential).then((success) => {
+        console.log("Firebase success: " + JSON.stringify(success));
+        //this.userProfile = success;
+      }).catch((error) => {
+        console.log("Firebase failure: " + JSON.stringify(error));
+      });
+    }).catch((error) => { console.log(error) });
+  }
 }
